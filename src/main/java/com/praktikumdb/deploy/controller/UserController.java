7cdd2450d424d877a0484268489d58464e7df910
halimbagusalhakim <halsat132@gmail.com>
2026-04-20 10:31:51 +0700

    membuat constructor untuk userController

---
+++ src/main/java/com/praktikumdb/deploy/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.praktikumdb.deploy.controller;
+
+import com.praktikumdb.deploy.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+}
